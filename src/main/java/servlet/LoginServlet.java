34472639f98225d9485313e080426d859dda6002
zhengye <dannyboygirl2010@gmail.com>
2013-12-10 19:51:32 +0100

    asd

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Entity.User;
+import Entity.UserDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			           throws ServletException, java.io.IOException {
+
+try
+{	    
+
+     User user = new User();
+     user.setNickname(request.getParameter("un"));
+     user.setPsw(request.getParameter("pw"));
+     System.out.print("1");
+     user = UserDAO.login(user);
+	 System.out.print("2");
+	   		
+     if (user.isValid())
+     {
+	        
+          HttpSession session = request.getSession(true);	    
+          session.setAttribute("currentSessionUser",user); 
+          response.sendRedirect("userLogged.jsp"); //logged-in page      		
+     }
+	        
+     else 
+          response.sendRedirect("invalidLogin.jsp"); //error page 
+} 
+		
+		
+catch (Throwable theException) 	    
+{
+     System.out.println(theException); 
+}
+       }
+	}
